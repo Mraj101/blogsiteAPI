b30cf446e844a1f41fc38f5d422e0c3650f51475
Hasnain <mrajhasnain101@gmail.com>
2024-03-07 13:01:21 +0600

    user modified

--- src/Core/services/user/user.js
+++ src/Core/services/user/user.js
@@ -3,12 +3,20 @@ const userModels = require("../../../models/user.models.js");
 // const Cryptography = require("./helperfuncitons/Cryptography");
 const { ApiError } = require("../../../utils/ApiError.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+//declaring global options
+const options = {
+  httpOnly: true,
+  secure: true,
+};
 
 //sigup user services
 async function create(data) {
   try {
     const { username, email, fullName, password } = data;
-    console.log(username, email, fullName, password, "data adsf");
+    // console.log(username, email, fullName, password, "data adsf");
     //checking if the feilds are empty
     if (
       [username, email, fullName, password].some(
@@ -25,7 +33,7 @@ async function create(data) {
     if (existsEmail) {
       throw new ApiError(409, "User with email or username already exists");
     }
-    console.log("hi exist");
+    // console.log("hi exist");
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
@@ -49,7 +57,7 @@ async function create(data) {
       email: user.email,
       name: user.username,
     };
-    console.log(payLoad, "pay");
+    // console.log(payLoad, "pay");
     return payLoad;
   } catch (err) {
     if (err instanceof ApiError) {
@@ -68,11 +76,13 @@ async function create(data) {
 //send cookies
 //send back the response.
 
+
+
 async function login(data) {
   try {
     // console.log(data, "data or not?");
     const { email, password } = data;
-    if ([email,password].some((feild)=>feild?.trim()==="")) {
+    if ([email, password].some((feild) => feild?.trim() === "")) {
       throw new ApiError(400, "user name or email required");
     }
     // console.log(email,password,"print");
@@ -87,22 +97,18 @@ async function login(data) {
       throw new ApiError(401, "invalid user credential");
     }
     //  console.log(email,"with what ")
-    const { accessToken, refreshToken, userInstance } =await TOKENS.generateAccessAndRefreshToknes(email);
+    const { accessToken, refreshToken, userInstance } =
+      await TOKENS.generateAccessAndRefreshToknes(email);
     // console.log(accessToken, "actokens");
-    // console.log(refreshToken, "retokens")
+    console.log(refreshToken, "retokens")
     // console.log(userInstance, "logged in");
     let modifiedUser = {
-      username: userInstance.username,
-      email: userInstance.email,
-      fullName:userInstance.fullName,
+      ...userInstance,
       accessToken,
       refreshToken,
     };
     // console.log(modifiedUser, "modified user");
-    const options = {
-      httpOnly: true,
-      secure: true,
-    };
+    
 
     return { options, modifiedUser };
 
@@ -122,13 +128,14 @@ async function login(data) {
     // const token = TOKENS.createAccessToken({ encryptedPayload });
     // return { userToken: token };
   } catch (err) {
-   if(err instanceof ApiError){
-    throw err;
-   }else{
-    throw new ApiError(500,"login service not available")
-   }
+    if (err instanceof ApiError) {
+      throw err;
+    } else {
+      throw new ApiError(500, "login service not available");
+    }
   }
 }
+
 
 async function logout(req, res) {
   try {
@@ -137,31 +144,61 @@ async function logout(req, res) {
     const user = await userModels.findByIdAndUpdate(
       req.user._id,
       {
-          $unset: {
-              refreshToken: 1 // this removes the field from document
-          }
+        $unset: {
+          refreshToken: 1, // this removes the field from document
+        },
       },
       {
-          new: true
+        new: true,
       }
-  )
-    if(!user){
-      throw new ApiError(404,"user not found");
+    );
+    if (!user) {
+      throw new ApiError(404, "user not found");
     }
-    const options={
-      httpOnly:true,
-      secure:true
-    }
-    return {};
+    return { options };
     // console.log(token,'token var in logout service');
   } catch (error) {
-    if(error instanceof ApiError){
+    if (error instanceof ApiError) {
       throw error;
-    }
-    else{
-      throw new ApiError(500,"logout service internal error")
+    } else {
+      throw new ApiError(500, "logout service internal error");
     }
   }
 }
 
-module.exports = { create, login, logout };
+
+async function tokenRefresh(req) {
+  // console.log(" inside refresh service ");
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "unauthorized request");
+  }
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    // console.log(decodedToken, "token decoded");
+    const user = await userModels.findById(decodedToken?._id);
+    console.log(user,"user fetch by token id");
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+    // console.log(incomingRefreshToken,"incoming token");
+    // console.log(user.refreshToken,"user.refreshtoken");
+    if (incomingRefreshToken !== user?.refreshToken) {
+      console.log("incoming and existing are not equal");
+      throw new ApiError(401, "Refresh token is expired or used");
+    }
+
+    // console.log("if i have user .id or not",user);
+    const { accessToken, refreshToken } = await TOKENS.generateAccessAndRefreshToknes(user.email);
+    console.log("no refresh token?")
+    return {accessToken,refreshToken,options}
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token");
+  }
+}
+
+module.exports = { create, login, logout, tokenRefresh };
