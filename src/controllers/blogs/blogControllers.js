1bf911dbc50f928cc9f7f5ca1da3877786a65d35
Hasnain <mrajhasnain101@gmail.com>
2024-03-10 18:44:25 +0600

    blog controllers

--- src/controllers/blogs/blogControllers.js
+++ src/controllers/blogs/blogControllers.js
@@ -1,6 +1,32 @@
+const blogServices = require("../../Core/services/blogs/index");
+const { ApiResponse } = require("../../utils/ApiResponse");
+const { ApiError } = require("../../utils/ApiError.js");
 
-async function creatBlogs(req,res){
-    console.log('hello');
+async function creatBlogs(req, res) {
+  try {
+    console.log("Controller", req.body);
+    console.log("hello");
+    let response = await blogServices.create(req.body);
+    return res
+      .status(201)
+      .json(new ApiResponse(200, response, "BLOGS created Successfully"));
+  } catch (err) {
+    // console.log(err);
+    // let newError = createErrorMessage();
+    // newError.status = 500;
+    // newError.message = "User Control Service Internal Server Error";
+    // return res.send(newError);
+    if (err instanceof ApiError) {
+      return res
+        .status(err.statusCode)
+        .json(new ApiResponse(err.statusCode, null, err.message));
+    } else {
+      console.error(err);
+      return res
+        .status(500)
+        .json(new ApiResponse(500, null, "Internal Server Error"));
+    }
+  }
 }
 
-module.exports={ creatBlogs }
+module.exports = { creatBlogs };
